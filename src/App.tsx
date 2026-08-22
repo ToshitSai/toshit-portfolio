a0448e6e3a705bda84a4f0488f1b5e936525ee8f
Toshit Sai <iamtoshitsai@gmail.com>
2026-08-22 16:03:41 +0530

    fix: completely remove custom scrollbar component from website

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CustomScrollbar from "@/components/portfolio/CustomScrollbar";
 import Index from "./pages/Index";
 import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
@@ -16,7 +15,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <CustomScrollbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<ContactPage />} />
